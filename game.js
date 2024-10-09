7f26c3b801690c7ac94e936f02920a360b313e0d
gokuweb5 <goku_web5@hotmail.com>
2024-10-09 01:21:05 +0000

    update1

--- game.js
+++ game.js
@@ -41,7 +41,7 @@ getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
     // go to the end page
-    return window.location.assign("/end.html");
+    return window.location.assign("/examen1/end.html");
   }
   questionCounter++;
   progressText.innerText = `Pregunta ${questionCounter}/${MAX_QUESTIONS}`;
